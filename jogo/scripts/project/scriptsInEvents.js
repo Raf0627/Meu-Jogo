1441d5d4e5d697a17e33b48250abd36e55144364
Raf0627 <rafaelfrossard076@gmail.com>
2025-04-07 21:30:41 -0300

    site base feito

---
+++ jogo/scripts/project/scriptsInEvents.js
@@ -0,0 +1,78 @@
+
+
+const scriptsInEvents = {
+
+	async FolhaDeEventosFase2_Event3_Act1(runtime, localVars)
+	{
+		let vidas = runtime.globalVars.Vidas;
+		
+		vidas = 0;
+		
+		runtime.globalVars.Vidas = vidas
+	},
+
+	async FolhaDeEventosFase1_Event8_Act1(runtime, localVars)
+	{
+		let vidas = runtime.globalVars.Vidas;
+		
+		vidas --;
+		
+		runtime.globalVars.Vidas = vidas
+	},
+
+	async FolhaDeEventosFase1_Event9_Act5(runtime, localVars)
+	{
+		let vidas = runtime.globalVars.Vidas;
+		
+		vidas --;
+		
+		runtime.globalVars.Vidas = vidas
+	},
+
+	async FolhaDeEventosFase1_Event13_Act1(runtime, localVars)
+	{
+		let vidas = runtime.globalVars.Vidas;
+		
+		vidas --;
+		
+		runtime.globalVars.Vidas = vidas
+	},
+
+	async FolhaDeEventosFase1_Event14_Act1(runtime, localVars)
+	{
+		let vidas = runtime.globalVars.Vidas;
+		
+		vidas --;
+		
+		runtime.globalVars.Vidas = vidas
+	},
+
+	async FolhaDeEventosFase1_Event17_Act2(runtime, localVars)
+	{
+		let vidas = runtime.globalVars.Vidas;
+		
+		vidas ++;
+		
+		runtime.globalVars.Vidas = vidas
+	},
+
+	async FolhaDeEventosFase1_Event21_Act1(runtime, localVars)
+	{
+		let vidas = runtime.globalVars.Vidas;
+		
+		vidas --;
+		
+		runtime.globalVars.Vidas = vidas
+	},
+
+	async FolhaDeEventosFase1_Event24_Act1(runtime, localVars)
+	{
+		let vidas = runtime.globalVars.Vidas;
+		
+		vidas --;
+		
+		runtime.globalVars.Vidas = vidas
+	}
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
